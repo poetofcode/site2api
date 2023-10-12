1d66ecc5499482f9bd7f618885f947f598a11e24
poetofcode <cyber-post@yandex.ru>
2023-10-12 18:19:58 +0300

    Implementing

--- app/index.js
+++ app/index.js
@@ -5,6 +5,8 @@ const repository = require('./repository')
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, label, printf } = format;
 const app = express();
 const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
 
@@ -31,16 +33,24 @@ class Application {
 	}
 
 	logger() {
+		const myFormat = printf(({ level, message, timestamp }) => {
+			const parsedDate = new Date(timestamp);
+			const dateFormatted = parsedDate.toISOString().
+  				replace(/T/, ' ').      // replace T with a space
+  				replace(/\..+/, '');
+		  return `${dateFormatted} ${level}: ${message}`;
+		});
 		return expressWinston.logger({
 	      transports: [
 	        new winston.transports.Console()
 	      ],
 	      format: winston.format.combine(
+	      	winston.format.timestamp(),
 	        winston.format.colorize(),
-	        winston.format.simple()
+	        myFormat
 	      ),
 	      meta: false, // optional: control whether you want to log the meta data about the request (default to true)
-	      msg: "HTTP {{req.method}} {{req.url}} {{res.responseTime}}ms", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+	      msg: "{{req.method}} {{req.url}} {{res.responseTime}}ms", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
 	      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
 	      colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
 	      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
