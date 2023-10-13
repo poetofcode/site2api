9db757c4ea469b95e37a3b8c9dd5c2dbd6b55b2d
poetofcode <cyber-post@yandex.ru>
2023-10-13 19:12:47 +0300

    Endpoints in progress

--- app/repository/endpoints.js
+++ app/repository/endpoints.js
@@ -1,3 +1,5 @@
+const ObjectId = require("mongodb").ObjectId;
+
 class EndpointRepository {
 
 	async fetchEndpoints(req, res) {
@@ -5,7 +7,79 @@ class EndpointRepository {
 	}
 
 	async createEndpoint(req, res) {
-		res.send({});
+		if(!req.body) {
+			return res.sendStatus(400);
+		}
+	    const collection = req.app.locals.db.collection("endpoints");
+		const snippetCollection = req.app.locals.db.collection('snippets');
+		const projectCollection = req.app.locals.db.collection('projects');
+
+		const url = req.body.url;
+		const snippets = req.body.snippets;
+		const method = req.body.method;
+		const name = req.body.name;
+		const projectId = req.params.projectId;
+
+		if (!url || url == 'undefined') {
+			return res.status(400).send('Field "url" undefined');
+		}
+		if (!snippets || !Array.isArray(snippets) || !(arr => snippets.every(i => typeof i === "string"))) {
+			return res.status(400).send('Field "snippets" undefined or not valid');
+		}
+		if (!method || method == 'undefined') {
+			return res.status(400).send('Field "method" undefined');
+		}
+
+		// Проверяем наличие projectId в базе
+		let _projectId = ""; 
+		try {
+			_projectId = new ObjectId(projectId);
+			const foundProjects = await projectCollection.find({ _id: _projectId }).toArray();
+			if (foundProjects.length == 0) {
+				return res.status(400).send(`Project with id '${projectId}' not found`);
+			}
+		}
+		catch(err) {
+			console.log(err);
+			return res.status(500).send(err);
+		}
+
+		// Проверяем наличие spinnet-ids в базе
+		let _snippetIds = [];
+		try {
+			_snippetIds = snippets.map(id => new ObjectId(id));
+			const foundSnippets = await snippetCollection.find({ _id: { $in: _snippetIds } }).toArray();
+			if (foundSnippets.length < snippets.length) {
+				return res.status(400).send('Not all snippet ids were found in the DB');
+			}
+		}
+		catch(err) {
+			console.log(err);
+			return res.status(500).send(err);
+		}
+
+		// Валидируем method
+		if (['get', 'post', 'patch', 'delete'].includes(method) == false) {
+			return res.status(400).send(`Method '${method}' not allowed`);			
+		}
+
+		const endpoint = {
+			projectId: _projectId,
+			url: url,
+			snippets: _snippetIds,
+			method: method
+		};
+		if (name && name != 'undefined') {
+			endpoint.name = name;
+		}
+
+	    try{
+	        await collection.insertOne(endpoint);
+	        res.send(endpoint);
+	    } 
+	    catch(err){
+	        res.status(500).send(err);
+	    }      		
 	}
 
 	async updateEndpoint(req, res) {
