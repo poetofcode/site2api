b09de26dd640aa98d115da43487851739f42ff02
poetofcode <cyber-post@yandex.ru>
2023-10-13 10:21:49 +0300

    Refactor

---
+++ app/utils/index.js
@@ -0,0 +1,34 @@
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, label, printf } = format;
+
+function logger() {
+	const myFormat = printf(({ level, message, timestamp }) => {
+		const parsedDate = new Date(timestamp);
+		const dateFormatted = parsedDate.toISOString().
+				replace(/T/, ' ').
+				replace(/\..+/, '');
+	  return `${dateFormatted} ${level}: ${message}`;
+	});
+	return expressWinston.logger({
+      transports: [
+        new winston.transports.Console()
+      ],
+      format: winston.format.combine(
+      	winston.format.timestamp(),
+        winston.format.colorize(),
+        myFormat
+      ),
+      meta: false,
+      msg: "{{req.method}} {{req.url}} {{res.responseTime}}ms",
+      expressFormat: true,
+      colorize: true,
+      ignoreRoute: function (req, res) { return false; }
+    })
+}
+
+exports.utils = {
+	logger: logger
+}
