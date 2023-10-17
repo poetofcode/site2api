ed49e8036f72953b6f1a56eb728ea42040fb4a06
poetofcode <cyber-post@yandex.ru>
2023-10-17 15:27:15 +0300

    Console in progress

--- app/api/endpoints.js
+++ app/api/endpoints.js
@@ -87,24 +87,6 @@ class EndpointMiddleware {
 		res.send({});
 	}
 
-	async fetchEndpointsAll(db) {
-	    const collection = db.collection("endpoints");
-		const snippetCollection = db.collection('snippets');
-		const projectCollection = db.collection('projects');
-
-		const endpoints = await collection.find({}).toArray();
-
-		const endpointsFull = endpoints.map(async (item) => {
-			const snippetsByItem = await snippetCollection.find({ _id: { $in : item.snippets } }).toArray();
-			const projectByEndpoint = await projectCollection.findOne({ _id: item.projectId });
-			item.snippets = snippetsByItem;
-			item.project = projectByEndpoint;
-			return item;
-		});
-
-		return await Promise.all(endpointsFull);
-	}
-
 	log(arr, title) {
 		console.log(`================ ${title}:`);
 		console.log(arr);
