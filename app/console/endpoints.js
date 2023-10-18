e759292bc1ebe0a6df9e5c3c7a000f2eb62a2d16
poetofcode <cyber-post@yandex.ru>
2023-10-18 19:01:35 +0300

    Console in progrress

--- app/console/endpoints.js
+++ app/console/endpoints.js
@@ -9,7 +9,8 @@ class EndpointMiddleware {
 			try {
 				const projectId = req.params.projectId;
 				const endpoints = (await this.context.apiGet(`/projects/${projectId}/endpoints`)).data.result;
-				res.render("endpoints.hbs", { endpoints: endpoints });
+				const project = (await this.context.apiGet(`/projects/${projectId}`)).data.result;
+				res.render("endpoints.hbs", { endpoints: endpoints, project: project });
 			} catch(err) {
 				next(err);
 			}
