446fe44cc62ccad42c7ebf0948d92d72a3152783
poetofcode <cyber-post@yandex.ru>
2023-10-19 20:58:25 +0300

    Console in progress

--- app/public/script/main.js
+++ app/public/script/main.js
@@ -6,3 +6,12 @@ function htmlDecode(input) {
   var doc = new DOMParser().parseFromString(input, "text/html");
   return doc.documentElement.textContent;
 }
+
+function codeEscape(buffer) {
+	const res = buffer
+		.replaceAll('\n', `\\n`)
+		.replaceAll('\t', `\\t`)
+		.replaceAll(`\r`, `\\r`)
+		.replaceAll(`"`, `\\"`);
+	return res;
+}
